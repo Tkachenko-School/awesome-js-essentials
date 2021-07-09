bdae2a2e0e33e7aa0a1048c111868777c48136a8
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-07-09 12:28:43 +0300

    11

---
+++ courses/part1/scripts.js
@@ -0,0 +1,13 @@
+const flat = [
+    { id: 1, parentId: 3 },
+    { id: 3, parentId: 8 },
+    { id: 4, parentId: 6 },
+    { id: 6, parentId: 3 },
+    { id: 7, parentId: 6 },
+    { id: 8, parentId: null },
+    { id: 10, parentId: 8 }, 
+    { id: 13, parentId: 14 },
+    { id: 14, parentId: 10 }
+  ]
+
+  
